6ff2adca9df1c8bec22eb0856f0318f6943816d9
rob4abcba <rob_abcba@yahoo.com>
2020-04-29 14:44:23 -0700

    Add dispatch for action creator setAuthedUser

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,13 +1,17 @@
 import { getInitialData } from '../utils/api'
-import { receiveUsers } from './users'
-import { receiveTweets } from './tweets'
+import { receiveUsers } from '../actions/users'
+import { receiveTweets } from '../actions/tweets'
+import { setAuthedUser } from '../actions/authedUser'
+
+const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData () {
-    return  (dispatch) => {
+    return (dispatch) => {
         return getInitialData()
         .then ( ({users,tweets}) => {
             dispatch(receiveUsers(users))
             dispatch(receiveTweets(tweets))
+            dispatch(setAuthedUser(AUTHED_ID))
         })
     }
 }
